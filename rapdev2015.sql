df916ca0940c60c0f5ad552e3363df4abcad7ed0
Matt <mjs7769@rit.edu>
2015-01-05 15:13:40 -0500

    Updated database

--- rapdev2015.sql
+++ rapdev2015.sql
@@ -15,8 +15,8 @@ CREATE  TABLE IF NOT EXISTS `Main`.`User` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(64) NOT NULL ,
   `password` VARCHAR(97) NOT NULL ,
-  `salt` VARCHAR(32) NOT NULL ,
   `email` VARCHAR(500) NOT NULL ,
+  `salt` VARCHAR(32) NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
@@ -28,42 +28,15 @@ DROP TABLE IF EXISTS `Main`.`Event` ;
 
 CREATE  TABLE IF NOT EXISTS `Main`.`Event` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `user_id` INT UNSIGNED NOT NULL ,
+  `owner_id` INT UNSIGNED NOT NULL ,
   `name` VARCHAR(256) NOT NULL ,
   `description` VARCHAR(5000) NOT NULL ,
-  `start_time` TIMESTAMP DEFAULT NOW() NOT NULL ,
-  `end_time` TIMESTAMP DEFAULT '1970-01-01 00:00:01' NOT NULL ,
-  `email_remind` TINYINT(1) NOT NULL ,
+  `start_time` TIMESTAMP NOT NULL DEFAULT NOW() ,
+  `end_time` TIMESTAMP NOT NULL DEFAULT '2020-02-02 02:02:02' ,
   PRIMARY KEY (`id`) ,
-  INDEX `id_idx` (`user_id` ASC) ,
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id` )
-    REFERENCES `Main`.`User` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Main`.`Share_Event`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Main`.`Share_Event` ;
-
-CREATE  TABLE IF NOT EXISTS `Main`.`Share_Event` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `event_id` INT UNSIGNED NOT NULL ,
-  `invited_id` INT UNSIGNED NOT NULL ,
-  `accepted` TINYINT(1) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `id_idx` (`event_id` ASC) ,
-  INDEX `id_idx1` (`invited_id` ASC) ,
-  CONSTRAINT `fk_event_id`
-    FOREIGN KEY (`event_id` )
-    REFERENCES `Main`.`Event` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invited_id`
-    FOREIGN KEY (`invited_id` )
+  INDEX `id_idx` (`owner_id` ASC) ,
+  CONSTRAINT `owner_user_id`
+    FOREIGN KEY (`owner_id` )
     REFERENCES `Main`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -82,8 +55,36 @@ CREATE  TABLE IF NOT EXISTS `Main`.`Tag` (
   `color` VARCHAR(6) NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `id_idx` (`user_id` ASC) ,
-  CONSTRAINT `fk_tag_user_id`
+  CONSTRAINT `tag_user_id`
     FOREIGN KEY (`user_id` )
+    REFERENCES `Main`.`User` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Main`.`Calendar_Item`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Main`.`Calendar_Item` ;
+
+CREATE  TABLE IF NOT EXISTS `Main`.`Calendar_Item` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `item_owner_id` INT UNSIGNED NOT NULL ,
+  `email_reminder` TINYINT(1) NULL ,
+  `event` INT UNSIGNED NOT NULL ,
+  `invited_by_id` INT UNSIGNED NOT NULL ,
+  `accepted` TINYINT(1) NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `item_owner_id_idx` (`item_owner_id` ASC) ,
+  INDEX `item_event_id_idx` (`invited_by_id` ASC) ,
+  CONSTRAINT `item_owner_id`
+    FOREIGN KEY (`item_owner_id` )
+    REFERENCES `Main`.`User` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `item_event_id`
+    FOREIGN KEY (`invited_by_id` )
     REFERENCES `Main`.`User` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -100,14 +101,14 @@ CREATE  TABLE IF NOT EXISTS `Main`.`Event_Tag` (
   `event_id` INT UNSIGNED NOT NULL ,
   `tag_id` INT UNSIGNED NOT NULL ,
   PRIMARY KEY (`id`) ,
-  INDEX `id_idx` (`event_id` ASC) ,
   INDEX `id_idx1` (`tag_id` ASC) ,
-  CONSTRAINT `fk_event_tag_event_id`
+  INDEX `id_idx` (`event_id` ASC) ,
+  CONSTRAINT `event_tag_event_id`
     FOREIGN KEY (`event_id` )
-    REFERENCES `Main`.`Event` (`id` )
+    REFERENCES `Main`.`Calendar_Item` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_event_tag_tag_id`
+  CONSTRAINT `event_tag_tag`
     FOREIGN KEY (`tag_id` )
     REFERENCES `Main`.`Tag` (`id` )
     ON DELETE NO ACTION
@@ -124,13 +125,13 @@ CREATE  TABLE IF NOT EXISTS `Main`.`Todo` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `user_id` INT UNSIGNED NOT NULL ,
   `name` VARCHAR(256) NOT NULL ,
-  `remind_time` TIMESTAMP DEFAULT NOW() NOT NULL ,
+  `remind_time` DATETIME NOT NULL ,
   `completed` TINYINT(1) NOT NULL ,
   `email_remind` TINYINT(1) NOT NULL ,
   `lapse_time` INT UNSIGNED NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `id_idx` (`user_id` ASC) ,
-  CONSTRAINT `fk_main_user_id`
+  CONSTRAINT `todo_user_id`
     FOREIGN KEY (`user_id` )
     REFERENCES `Main`.`User` (`id` )
     ON DELETE NO ACTION
@@ -150,12 +151,12 @@ CREATE  TABLE IF NOT EXISTS `Main`.`Todo_Tag` (
   PRIMARY KEY (`id`) ,
   INDEX `id_idx` (`tag_id` ASC) ,
   INDEX `id_idx1` (`todo_id` ASC) ,
-  CONSTRAINT `fk_todo_tag_tag_id`
+  CONSTRAINT `todo_tag_tag_id`
     FOREIGN KEY (`tag_id` )
     REFERENCES `Main`.`Tag` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_todo_tag_todo_id`
+  CONSTRAINT `todo_tag_todo_id`
     FOREIGN KEY (`todo_id` )
     REFERENCES `Main`.`Todo` (`id` )
     ON DELETE NO ACTION
@@ -177,7 +178,7 @@ CREATE  TABLE IF NOT EXISTS `Main`.`Subtask` (
   `lapse_time` INT UNSIGNED NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `id_idx` (`parent_id` ASC) ,
-  CONSTRAINT `fk_subtask_parent_id`
+  CONSTRAINT `subtask_parent_id`
     FOREIGN KEY (`parent_id` )
     REFERENCES `Main`.`Todo` (`id` )
     ON DELETE NO ACTION
