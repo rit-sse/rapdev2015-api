1f93532d4cf2e77e2ec758338b282a5f993088d7
kristenmills <kristen@kristen-mills.com>
2015-01-06 11:39:06 -0500

    todo

---
+++ api/routes/todo.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+router
+  .route('/')
+    .get(function(req, res, next) {
+
+    })
+    .post(function(req, res, next) {
+
+    });
+
+router
+  .route('/:id')
+    .get(function(req, res, next) {
+
+    })
+    .put(function(req, res, next) {
+
+    })
+    .delete(function(req, res, next) {
+
+    });
+
+router
+  .route('/:id/complete')
+    .put(function(req, res, next) {
+
+    });
+
+router
+  .route('/:id/reopen')
+    .put(function(req, res, next) {
+
+    });
+
+router
+  .route('/:id/subtasks')
+    .get(function(req, res, next) {
+
+    })
+    .post(function(req, res, next) {
+
+    });
+
+module.exports = function(app) {
+  app.use('/api/todo', router);
+}
