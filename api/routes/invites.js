b97efa3554c2c62d50bf0e023b16334214c974ba
kristenmills <kristen@kristen-mills.com>
2015-01-06 12:02:18 -0500

    Invites

---
+++ api/routes/invites.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+router
+  .route('/:id/accept')
+    .post(function(req, res, next){
+
+    });
+
+router
+  .route('/:id/decline')
+    .post(function(req, res, next){
+
+    });
+
+module.exports = function(app) {
+  app.use('/api/invite', router);
+}
