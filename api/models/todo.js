c883c2a16341f0416c5fb5c3ac875239176ae353
Ben Kantor <bkan@bn.co>
2015-01-06 14:45:02 -0500

    adds todo model

---
+++ api/models/todo.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = function (sequelize, DataTypes) {
+  var Todo = sequelize.define("Todo", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    remind_time: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    email_remind: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    lapse_time: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false
+    }
+  },
+  {
+    classMethods: {
+      associate: function (models) {
+        Todo.belongsTo(models.User);
+        Todo.belongsTo(models.Todo.id, {as: 'parent'});
+        Todo.hasMany(models.Tag);
+      },
+
+    }
+  });
+  return Todo;
+}
