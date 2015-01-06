210940aff0508ed828b84df885f224c99a52f948
Omodi <sjg8961@rit.edu>
2015-01-06 14:04:11 -0500

    Add user

---
+++ api/models/user.js
@@ -0,0 +1,26 @@
+"use strict";
+var Sequelize = require('sequelize')
+
+module.exports = function(sequelize, DataTypes){
+  var User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING(64),
+      allowNull: false},
+    email: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+      validate: {
+        isEmail: true}
+      }
+    }, {
+    classMethods: {
+      associate : function(models) {
+        User.hasMany(model.CalendarItem.id),
+        User.hasMany(model.Todo.id),
+        User.hasMany(model.Tag.id),
+        User.hasMany(model.Event.id)
+      }
+    }
+  });
+  return User;
+};
