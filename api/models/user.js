8dd037cbf475437284d209f995b56941e5b19877
kristenmills <kristen@kristen-mills.com>
2015-01-06 15:19:02 -0500

    formatting

--- api/models/user.js
+++ api/models/user.js
@@ -4,16 +4,14 @@ var Sequelize = require('sequelize')
 
 module.exports = function(sequelize, DataTypes){
   var User = sequelize.define('User', {
-    username: {
-      type: DataTypes.STRING(64),
-      allowNull: false},
     email: {
       type: DataTypes.STRING(500),
       allowNull: false,
       validate: {
-        isEmail: true}
+        isEmail: true
       }
-    }, {
+    }
+  }, {
     classMethods: {
       associate : function(models) {
         User.hasMany(models.CalendarItem, { as: 'ItemOwner'});
