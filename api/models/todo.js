575497d7b460dc5d9276c72756016d31cc2cdce1
mrm9084 <mrm9084@rit.edu>
2015-01-12 14:08:07 -0500

    added methods to routes/todo.js and removed services/todo.js

--- api/models/todo.js
+++ api/models/todo.js
@@ -25,6 +25,95 @@ module.exports = function(db, models) {
     Todo.hasOne('user', models.user, { reverse: 'todos' })
     Todo.hasOne('parent', Todo, {reverse: 'subtasks'});
   }
+  
+  Todo.getAllTodosOfUser = function(request,callback){
+    request.models.todo.find({user_id:request.user.id},function(err,results){
+      callback(err,results);
+    });
+  },
 
+  Todo.createNewTodo = function(name,dueDate,request,callback){
+    var newTodo = {};
+    newTodo.name = name;
+    newTodo.dueDate = dueDate;
+    newTodo.completed = false;
+    newTodo.lapseTime = 0;
+    db.models.todo.create(newTodo,function(err,result){
+      newTodo.setUser(request.user.id);
+      result.save(function(err){
+          callback(err,result);
+      });
+    });
+  },
+
+  Todo.getTodoById = function(todoId,results,callback){
+    results.models.todo.get(todoId, funtion(err,result){
+      callback(err,result);
+    });
+  },
+
+  Todo.editTodoById = function(todoId,todo,request,callback){
+    request.models.todo.get(todoId, funtion(err,result){
+      newTodo.name = todo.name;
+      newTodo.dueDate = todo.dueDate;
+      newTodo.completed = todo.completed;
+      newTodo.lapseTime = todo.lapseTime;
+      result.save(function(err){
+        newTodo.setParent(todo.getParent());
+        newTodo.setUser(todo.getUser());
+        result.save(function(err){
+          callback(err,result);
+        }
+      });
+    });
+  },
+
+  Todo.removeTodoById = function(todoId,request,callback){
+    db.models.Todo.get(todoId, funtion(err,result){
+      result.remove(function(err){
+          callback(err);
+      });
+    });
+  },
+
+  Todo.getChildrenOfTodoById = function(todoId,request,callback){
+    request.models.todo.find({parent:todoId},function(err,results){
+      callback(err,result);
+    });
+  },
+
+  Todo.createNewChildTodo = function(parent,name,dueDate,request,callback){
+    var newTodo = {};
+    newTodo.name = name;
+    newTodo.dueDate = dueDate;
+    newTodo.completed = false;
+    newTodo.lapseTime = 0;
+    request.models.todo.create(newTodo,function(err,result){
+      newTodo.setUser(request.user.id);
+      newTodo.setParent(parent);
+      result.save(function(err){
+          callback(err,result);
+      });
+    });
+  },
+
+  Todo.completeTaskById = function(todoId,request,callback){
+    request.models.todo.get(todoId, funtion(err,result){
+      result.completed = true;
+      result.save(function(err){
+          callback(err);
+      });
+    });
+  },
+
+  Todo.reopenTaskById = function(todoId,request,callback){
+    request.models.todo.get(todoId, funtion(err,result){
+      result.completed = false;
+      result.save(function(err){
+          callback(err);
+      });
+    });
+  }
+  
   models.todo = Todo;
 }
--- api/routes/todo.js
+++ api/routes/todo.js
@@ -4,43 +4,67 @@ var router = express.Router();
 router
   .route('/')
     .get(function(req, res, next) {
-
+    
+      req.todo.getAllTodosOfUser(function(result){
+        req.send(result);
+      });
     })
     .post(function(req, res, next) {
-
+      var body = req.body;
+      req.todo.createNewTodo(body.name,body.dueDate,req,function(result){
+        req.send(result);
+      });
     });
 
 router
   .route('/:id')
     .get(function(req, res, next) {
-
+      var body = req.body;
+      req.todo.getTodoById(body.todoId,req,function(result){
+        req.send(result);
+      });
     })
     .put(function(req, res, next) {
-
+      var body = req.body
+      req.todo.editTodoById(body.todoId,body.todo,req,function(result){
+        req.send(result);
+      });
     })
     .delete(function(req, res, next) {
-
+      var body = req.body;
+      req.todo.removeTodoById(body.todoId,req,function(result){
+        req.send(result);
+      });
     });
 
 router
   .route('/:id/complete')
     .put(function(req, res, next) {
-
+      req.todo.completeTaskById(req.body.todoId,req,function(result){
+        req.send(result);
+      });
     });
 
 router
   .route('/:id/reopen')
     .put(function(req, res, next) {
-
+      req.todo.reopenTaskById(req.body.todoId,req,function(result){
+        req.send(result);
+      });
     });
 
 router
   .route('/:id/subtasks')
     .get(function(req, res, next) {
-
+      req.todo.getChildrenOfTodoById(req.body.todoId,req,function(result){
+        req.send(result);
+      });
     })
     .post(function(req, res, next) {
-
+      var body = req.body;
+      req.todo.createNewChildTodo(body.parent,body.name,body.dueDate,req,function(result){
+        req.send(result);
+      });
     });
 
 router
