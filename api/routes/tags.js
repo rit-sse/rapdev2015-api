c9568d9688adcf73a3752404bf7b25862c09d6fc
Matt Smicinski <mjs7769@rit.edu>
2015-01-12 09:46:09 -0800

    remove logs

--- api/routes/tags.js
+++ api/routes/tags.js
@@ -4,13 +4,10 @@ var router = express.Router();
 router
   .route('/')
     .get(function(req, res, next) {
-      console.log(req.user.id);
       req.models.tag.find({user_id: req.user.id}, function(err, tags) {
-        console.log(tags[0]);
         var userTags = []
         for(var i = 0; i < tags.length; i++) {
           var tag = tags[i];
-          console.log(tags[i]);
           userTags.push({name:tag["name"], color:tag["color"]});
         }
         res.send(userTags);
