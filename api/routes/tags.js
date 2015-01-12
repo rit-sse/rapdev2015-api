08e35dcb5e7d12e4925e0a0c6325cd03a79a8da9
Matt Smicinski <mjs7769@rit.edu>
2015-01-12 10:14:07 -0800

    Delete tags

--- api/routes/tags.js
+++ api/routes/tags.js
@@ -22,28 +22,38 @@ router
 router
   .route('/:id')
     .get(function(req, res, next) {
-      console.log(req.params.id);
       req.models.tag.get(req.params.id, function(err, tag) {
-          res.send({id:tag["id"], name:tag["name"], color:tag["color"]});
+          if(err) {
+            res.send({result:"ERROR"});
+          } else {
+            res.send({id:tag["id"], name:tag["name"], color:tag["color"]});
+          }
       });
     })
     .put(function(req, res, nex) {
-      console.log(req.body);
       req.models.tag.get(req.params.id, function(err, Tag) {
         var newName = req.body.name;
         var newColor = req.body.color;
         Tag.save({name:newName, color:newColor}, function(err) {
           if(err) {
-            res.send({status:"ERROR"});
+            res.send({result:"ERROR"});
           } else {
-            res.send({status:"SUCCESS"});
+            res.send({result:"SUCCESS"});
           }
         });
       });
       
     })
     .delete(function(req, res, next) {
-
+      req.models.tag.get(req.params.id, function(err, Tag) {
+        Tag.remove(function(err) {
+          if(err) {
+            res.send({result:"ERROR"});
+          } else {
+            res.send({result:"SUCCESS"});
+          }
+        });
+      });
     });
 
 module.exports = function(app) {
