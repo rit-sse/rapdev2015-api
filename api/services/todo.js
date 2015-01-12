7ad0e04bd9ee07fcd903fb4abd232e2a79f5f4a3
mrm9084 <mrm9084@rit.edu>
2015-01-12 13:32:16 -0500

    Updated changes to todo requests

--- api/services/todo.js
+++ api/services/todo.js
@@ -4,43 +4,49 @@ var orm = require('orm');
 
 
 module.exports = {
-  getAllTodosOfUser : function(userId,db,callback){
-    db.models.Todo.find({id:userId},function(err,results){
+  getAllTodosOfUser : function(userId,request,callback){
+    request.models.todo.find({user_id:request.user.id},function(err,results){
       callback(err,results);
     });
   },
 
-  createNewTodo : function(name,remind_time,email_remind,db,callback){
+  createNewTodo : function(name,dueDate,request,callback){
     var newTodo = {};
     newTodo.name = name;
-    newTodo.remind_time = remind_time;
-    newTodo.email_remind = email_remind;
-    db.models.Todo.create(newTodo,function(err,result){
-      callback(err,result);
-    });
-  },
-
-  getTodoById : function(todoId,db,callback){
-    db.models.Todo.get(todoId, funtion(err,result){
-      callback(err,result);
-    });
-  },
-
-  editTodoById : function(todoId,todo,db,callback){
-    db.models.Todo.get(todoId, funtion(err,result){
-      result.name = todo.name;
-      result.remind_time = todo.remind_time;
-      result.completed = todo.completed;
-      result.email_remind = todo.email_remind;
-      result.lapse_time = todo.lapse_time;
-      result.parent = todo.parent;
+    newTodo.dueDate = dueDate;
+    newTodo.completed = false;
+    newTodo.lapseTime = 0;
+    db.models.todo.create(newTodo,function(err,result){
+      newTodo.setUser(request.user.id);
       result.save(function(err){
-          callback(err);
+          callback(err,result);
       });
     });
   },
 
-  removeTodoById : function(todoId,db,callback){
+  getTodoById : function(todoId,results,callback){
+    results.models.todo.get(todoId, funtion(err,result){
+      callback(err,result);
+    });
+  },
+
+  editTodoById : function(todoId,todo,request,callback){
+    request.models.todo.get(todoId, funtion(err,result){
+      newTodo.name = todo.name;
+      newTodo.dueDate = todo.dueDate;
+      newTodo.completed = todo.completed;
+      newTodo.lapseTime = todo.lapseTime;
+      result.save(function(err){
+        newTodo.setParent(todo.getParent());
+        newTodo.setUser(todo.getUser());
+        result.save(function(err){
+          callback(err,result);
+        }
+      });
+    });
+  },
+
+  removeTodoById : function(todoId,request,callback){
     db.models.Todo.get(todoId, funtion(err,result){
       result.remove(function(err){
           callback(err);
@@ -48,25 +54,29 @@ module.exports = {
     });
   },
 
-  getChildrenOfTaskById : function(todoId,db,callback){
-    db.models.Todo.find({parent:todoId},function(err,results){
+  getChildrenOfTaskById : function(todoId,request,callback){
+    request.models.todo.find({parent:todoId},function(err,results){
       callback(err,result);
     });
   },
 
-  createNewChildTodo : function(parent,name,remind_time,email_remind,db,callback){
+  createNewChildTodo : function(parent,name,dueDate,request,callback){
     var newTodo = {};
     newTodo.name = name;
-    newTodo.remind_time = remind_time;
-    newTodo.email_remind = email_remind;
-    newTodo.parent = parent.id;
-    db.models.Todo.create(newTodo,function(err,result){
-      callback(err,result);
+    newTodo.dueDate = dueDate;
+    newTodo.completed = false;
+    newTodo.lapseTime = 0;
+    request.models.todo.create(newTodo,function(err,result){
+      newTodo.setUser(request.user.id);
+      newTodo.setParent(parent);
+      result.save(function(err){
+          callback(err,result);
+      });
     });
   },
 
-  completeTaskById : function(todoId,db,callback){
-    db.models.Todo.get(todoId, funtion(err,result){
+  completeTaskById : function(todoId,request,callback){
+    request.models.todo.get(todoId, funtion(err,result){
       result.completed = true;
       result.save(function(err){
           callback(err);
@@ -74,8 +84,8 @@ module.exports = {
     });
   },
 
-  reopenTaskById : function(todoId,db,callback){
-    db.models.Todo.get(todoId, funtion(err,result){
+  reopenTaskById : function(todoId,request,callback){
+    request.models.todo.get(todoId, funtion(err,result){
       result.completed = false;
       result.save(function(err){
           callback(err);
