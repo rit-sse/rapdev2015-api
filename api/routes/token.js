e5da28d95cbfe8e57dc4291e8d4802ae20a7c051
Wesley Wigham <wwigham@gmail.com>
2015-01-13 11:15:46 -0500

    refresh tokens

--- api/routes/token.js
+++ api/routes/token.js
@@ -15,6 +15,36 @@ module.exports = function(app, secret) {
     res.statusCode = 401;
 
     switch (provider) {
+      case 'refresh_token': {
+        var token;
+        if (req.headers && req.headers.authorization) {
+          var parts = req.headers.authorization.split(' ');
+          if (parts.length == 2) {
+            var scheme = parts[0];
+            var credentials = parts[1];
+
+            if (/^Bearer$/i.test(scheme)) {
+              token = credentials;
+            }
+          } else {
+            return res.send({error: 'bad authorization header format'}); //bad format
+          }
+        }
+        
+        if (!token) {
+          return res.send({error: 'couldn\'t find token in authorization header'}); //no token
+        }
+        
+        jwt.verify(token, secret, {}, function(err, decoded) {
+          if (err) return res.send({error: 'invalid token'});
+
+          var response = jwt.sign(decoded, secret, {expiresInMinutes: expiresIn});
+
+          res.statusCode = 200;
+          res.send({token: response, exp: new Date((new Date()).getTime() + expiresIn*60000) });
+        });
+        break;
+      }
       case 'facebook': {
           rq('https://graph.facebook.com/me?access_token='+req.param('token')).then(function(resp) {
             resp = JSON.parse(resp);
