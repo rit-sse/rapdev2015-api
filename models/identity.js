b2d8df46a7a96c5ba028ab95c48400bd06396cd4
Tina Howard <chh2538@rit.edu>
2015-01-16 14:36:30 -0500

    switched where the idnentities were

--- models/identity.js
+++ models/identity.js
@@ -29,6 +29,40 @@ var Identity = bookshelf.Model.extend({
     return this.hasMany('Todo');
   }
 
+},{
+  createIdentity: function(name,singular,user,cb){
+    Identity
+    .forge({name:name, singular:singular, members:[user]})
+    .save()
+    .then(cb);
+  },
+  updateIdentity: function(identityId, name, singular, memberIds, cb){
+    var members = new Collection(memberIds)
+      .mapThen(function(memberId){
+        User
+          .where({id: memberId})
+          .fetch()
+          .then(function(member){
+            return member;
+          })
+      });
+    Identity
+        .where({id: identityId})
+        .fetch()
+        .set({
+          name: name,
+          singular: singular,
+          members: members
+        });
+  },
+  returnIdentity: function(identityId, cb){
+    Identity
+      .where({id: identityId})
+      .fetch()
+      .set("url", "/identities/" + identityId)
+      .then(cb);
+  }
 });
+
 
 module.exports = bookshelf.model('Identity', Identity);
